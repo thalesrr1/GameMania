a493927865dc528d561f212eccd5b7a9adb3f272
Thales <thalesroberto.f@hotmail.com>
2024-09-19 20:35:39 -0300

    it was necessary to create the routing file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { ListaComponent } from './lista/lista.component';
+import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    ListaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lista/lista.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ProdutoService } from '../produto.service';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent {
+
+public produtos: any[] = []
+
+constructor(private produtoService: ProdutoService) {}
+
+ngOnInit(): void {
+  this.listarProdutos()
+}
+
+listarProdutos() {
+  this.produtoService.getProdutos().subscribe(
+    produtosDaApi => {
+      this.produtos = produtosDaApi
+    }
+  )
+}
+
+}
